3baf969fc072f04fb71ded8b5dd6f46a1d5a8a57
Fahad Almutairi <88871860+iLawFD@users.noreply.github.com>
2023-01-27 08:30:38 +0300

    Update fll.py

--- fll.py
+++ fll.py
@@ -257,7 +257,7 @@ async def t221(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
    voter = 1 
    for i in listObject[0]:
         if i.containsFigure():
-                await context.bot.send_photo(update.effective_chat.id,photo = open("images\\"+ i.getFigureName(), "rb"))
+               await context.bot.send_photo(update.effective_chat.id,photo = open(os.path.join("images" , i.getFigureName()), 'rb'))
         pos =  correctAnsPos(i.getAns() , i.getCorrectAns())
         quest = i.getQuestion()
         exp = ""
